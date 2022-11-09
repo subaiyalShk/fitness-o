fd1ca21e48b7157bceb3c1a7b6ef53a4b98540a9
Subaiyal Sheikh <subaiyalshk@gmail.com>
2022-11-08 20:05:54 -0600

    env variables added

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,17 +6,18 @@ import {getAuth} from 'firebase/auth'
 import {getFirestore} from '@firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+require('dotenv').config()
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA4NdxyEAKrVU8TEtCCJojZRcxMHQZRS_Q",
-  authDomain: "fitness-app-944fc.firebaseapp.com",
-  projectId: "fitness-app-944fc",
-  storageBucket: "fitness-app-944fc.appspot.com",
-  messagingSenderId: "978327309093",
-  appId: "1:978327309093:web:70c5f1616567931ab3a8cc",
-  measurementId: "G-EDVH8RSJPF"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID
 };
 
 // Initialize Firebase
